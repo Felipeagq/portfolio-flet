8e20849ecae0ae45108b8d758a925118fcefd812
Felipeagq <felipeagq99@gmail.com>
2022-11-15 17:11:29 -0500

    [MODIFY] un comment

--- entrypoint.py
+++ entrypoint.py
@@ -14,70 +14,70 @@ from components.history_component import HistoryComponentEducate, HistoryCompone
 
 
 def main(page: Page):
-    # Configuraciones    
+    Configuraciones    
     page.title = "Felipe Portfolio"
     page.padding = 50
     page.bgcolor = colors.BROWN_400
     page.scroll = "hidden"
 
     page.add(Text("Hola"))
-    # Componentes
-    # page.add(
-    #     Column([
-    #         Container(content=Column(
+    Componentes
+    page.add(
+        Column([
+            Container(content=Column(
                 
-    #             controls=[
-    #                 PhotoStack("/assets/felipe.jpeg"),
-    #                 TextComponent(),
-    #                 ResumeComponent(),
-    #                 # IconComponents(page),
-    #                 IconComponentsTwo(page),
-    #                 GridComponent([
-    #                     HabilityComponent(
-    #                         icons.MANAGE_ACCOUNTS,
-    #                         "Project Manager",
-    #                         "Todo en la vida tiene un orden, incluso los proyectos"
-    #                         ),
-    #                     HabilityComponent(
-    #                         icons.STORAGE_OUTLINED,
-    #                         "BackEnd Development",
-    #                         "Gestion de la logica de negocio de un proyecto y base de datos"
-    #                         ),
-    #                     HabilityComponent(
-    #                         icons.COMPUTER_SHARP,
-    #                         "DevOps",
-    #                         "Despliegue de proyectos con tecnologias en la vanguardia"
-    #                         ),
-    #                             #--
-    #                 ]),
-    #                 GridComponent([
-    #                     HistoryComponentEducate(),
-    #                     HistoryComponentWork()
-    #                     ]),
-    #                 GridComponent([
-    #                     TecnologiesComponent("logo-teal.png"),
-    #                     TecnologiesComponent("aws.png"),
-    #                     TecnologiesComponent("flask-bg.png"),
-    #                     TecnologiesComponent("trello.png"),
-    #                     TecnologiesComponent("git.png"),
-    #                     TecnologiesComponent("python.png"),
-    #                     TecnologiesComponent("flet.png"),
-    #                     TecnologiesComponent("drf.png"),
-    #                     TecnologiesComponent("docker.png"),
-    #                             #--
-    #                 ])
-    #             ],
-    #             horizontal_alignment="center",
-    #             spacing=55,
-    #             # alignment="center",
+                controls=[
+                    PhotoStack("/assets/felipe.jpeg"),
+                    TextComponent(),
+                    ResumeComponent(),
+                    # IconComponents(page),
+                    IconComponentsTwo(page),
+                    GridComponent([
+                        HabilityComponent(
+                            icons.MANAGE_ACCOUNTS,
+                            "Project Manager",
+                            "Todo en la vida tiene un orden, incluso los proyectos"
+                            ),
+                        HabilityComponent(
+                            icons.STORAGE_OUTLINED,
+                            "BackEnd Development",
+                            "Gestion de la logica de negocio de un proyecto y base de datos"
+                            ),
+                        HabilityComponent(
+                            icons.COMPUTER_SHARP,
+                            "DevOps",
+                            "Despliegue de proyectos con tecnologias en la vanguardia"
+                            ),
+                                #--
+                    ]),
+                    GridComponent([
+                        HistoryComponentEducate(),
+                        HistoryComponentWork()
+                        ]),
+                    GridComponent([
+                        TecnologiesComponent("logo-teal.png"),
+                        TecnologiesComponent("aws.png"),
+                        TecnologiesComponent("flask-bg.png"),
+                        TecnologiesComponent("trello.png"),
+                        TecnologiesComponent("git.png"),
+                        TecnologiesComponent("python.png"),
+                        TecnologiesComponent("flet.png"),
+                        TecnologiesComponent("drf.png"),
+                        TecnologiesComponent("docker.png"),
+                                #--
+                    ])
+                ],
+                horizontal_alignment="center",
+                spacing=55,
+                # alignment="center",
                 
-    #         ),
-    #         width=page.width
-    #         # width=page.window_width
-    #         )
-    #     ],
-    # )
-    # )
+            ),
+            width=page.width
+            # width=page.window_width
+            )
+        ],
+    )
+    )
     page.update()
 
 
